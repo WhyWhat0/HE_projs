6e87961025be01590df581b544470dd93bd62cf3
WhyWhat0 <108304655+WhyWhat0@users.noreply.github.com>
2023-02-09 23:01:07 +0300

    Добавьте файлы проекта.

---
+++ Menu/Header.h
@@ -0,0 +1,10 @@
+#include <conio.h>
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+#include<windows.h>
+
+using namespace std;
+
+
+void lab2_1(); void lab2_2(); void lab3_1(); void lab3_2(); void lab4(); void lab5();
---
+++ testcode/testcode.cpp
@@ -0,0 +1,15 @@
+﻿#include <conio.h>
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+#include<windows.h>
+
+using namespace std;
+
+int main()
+{
+	short a = 707, b = 10;
+	bool c = a & b == a;
+	cout << c;
+	return 0;
+}
