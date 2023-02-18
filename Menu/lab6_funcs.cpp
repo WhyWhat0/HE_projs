ccbbbba3182c10fc3f70831f266fc439aee70ea6
WhyWhat0 <108304655+WhyWhat0@users.noreply.github.com>
2023-02-18 21:43:17 +0300

    путь

--- Menu/lab6_funcs.cpp
+++ Menu/lab6_funcs.cpp
@@ -19,7 +19,7 @@ void move(int x, int y) {
 }
 void check_wall(int &x, int &y, int map[], int c) {
 	unsigned int Bit = 0b1000'0000'0000'0000'0000'0000'0000'0000;
-	if ((Bit >> x) & map[y]) {
+	if ((Bit >> x) & map[y] || x == -1 || y == -1) {
 		Beep(400, 100);
 		switch (c) {
 		case 72:
@@ -43,7 +43,6 @@ bool end(int x, int y, int ex, int ey) {
 		SetColor(5, 15);
 		system("cls");		
 		cout << "Easy win!!!" << endl;
-		SetColor(5, 5);
 		_getch();
 		return true;
 	}
@@ -63,24 +62,28 @@ bool is_visited(int nx, int ny, block* visited, int col_ways) {
 	for (int i = 0;i < col_ways;i++) if (visited[i].x == nx && visited[i].y == ny) return true;
 	return false;
 }
+void out_vis(block* visited, int col_ways) {
+	ofstream f("text.txt");
+	for (int i = 0; i < col_ways; i++) f << i + 1 << ") " << visited[i].x << " " << visited[i].y << endl;
+	f.close();
+}
 
-int find_way(int nx, int ny, block* visited, int *map, int col_ways, int &i) {
-	
+int find_way(int nx, int ny, block* visited, int *map, int col_ways, int i = 0) {
+	visited[i].x = nx; visited[i].y = ny; i++;
 	unsigned int Bit = 0b1000'0000'0000'0000'0000'0000'0000'0000;
-	cout << "основное: " << i << " вызов (RLDU) " << !((Bit >> nx + 1) & map[ny]) <<
+	/*cout << "основное: " << i << " вызов (RLDU) " << !((Bit >> nx + 1) & map[ny]) <<
 		!((Bit >> nx - 1) & map[ny]) << !((Bit >> nx) & map[ny + 1]) <<
-		!((Bit >> nx) & map[ny - 1]) << "    " << nx << " " << ny << "   " << stop(nx, ny) << endl; //out_vis(visited, col_ways); cout << endl;
+		!((Bit >> nx) & map[ny - 1]) << "    " << nx << " " << ny << "   " << stop(nx, ny) << endl;*/ //out_vis(visited, col_ways); cout << endl;
 	//Sleep(500);
 	if (nx == 0 && ny == 14) {
 		return 1;
 	}
 	if (!stop(nx, ny)) {
-		visited[i].x = nx; visited[i].y = ny; i++;
 
-		cout << "Доп. " << visited[i].x << " " << visited[i].y << "." << " " << i << " " << endl;
+		//cout << "Доп. " << visited[i].x << " " << visited[i].y << "." << " " << i << " " << endl;
 		
 		if (!((Bit >> nx + 1) & map[ny]) && !is_visited(nx + 1, ny, visited, col_ways)) {
-			if (find_way(nx + 1, ny, visited, map, col_ways, i)) return 1;;
+			if (find_way(nx + 1, ny, visited, map, col_ways, i)) return 1;
 		}
 		if (!((Bit >> nx - 1) & map[ny]) && !is_visited(nx - 1, ny, visited, col_ways)) {
 			if(find_way(nx - 1, ny, visited, map, col_ways, i)) return 1;
@@ -89,9 +92,21 @@ int find_way(int nx, int ny, block* visited, int *map, int col_ways, int &i) {
 			if(find_way(nx, ny + 1, visited, map, col_ways, i)) return 1;
 		}
 		if (!((Bit >> nx) & map[ny - 1]) && !is_visited(nx, ny - 1, visited, col_ways)) {
-			if(find_way(nx, ny - 1, visited, map, col_ways,  i)) return 1;
+			if(find_way(nx, ny - 1, visited, map, col_ways, i)) return 1;
 		}
 	}
 	
 	return 0;
+}
+void mark(block* visited, int nex, int ney) {
+	SetColor(2, 2);
+	int i = 0;
+	while (!(visited[i].x == nex && visited[i].y == ney)) {
+		Sleep(50);
+		point(visited[i].x, visited[i].y);
+		cout << "  ";
+		i++;
+	}
+	point(visited[i].x, visited[i].y);
+	cout << "  ";
 }
